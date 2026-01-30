7edb9a98add0c47bac0da34e26a30dad7e3a6fa3
Ilia Munaev <ilyamunaev@gmail.com>
2026-01-30 18:28:33 +0200

    Move delayForStep to separate file

---
+++ internal/service/delay.go
@@ -0,0 +1,13 @@
+package service
+
+import "time"
+
+func delayForStep(delayMS map[string]int64, step string, defaultDelay time.Duration) time.Duration {
+	if delayMS == nil {
+		return defaultDelay
+	}
+	if ms, ok := delayMS[step]; ok && ms > 0 {
+		return time.Duration(ms) * time.Millisecond
+	}
+	return defaultDelay
+}
